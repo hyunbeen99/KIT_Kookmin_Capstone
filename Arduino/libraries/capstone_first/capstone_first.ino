5ba8d381f3025ed6565d89e3ef09178eb7d2dc61
KIT <hyunbeen99>
2021-05-31 19:02:45 +0900

    Arduino

--- Arduino/libraries/capstone_first/capstone_first.ino
+++ Arduino/libraries/capstone_first/capstone_first.ino
@@ -1,3 +1,4 @@
+/*
 #include <HttpClient.h>
 #include <YunServer.h>
 #include <BridgeServer.h>
@@ -10,6 +11,7 @@
 #include <BridgeUdp.h>
 #include <Console.h>
 #include <BridgeSSLClient.h>
+*/
 
 #include <ros.h>
 
@@ -17,33 +19,51 @@
 #include <geometry_msgs/Twist.h>
 
 ros::NodeHandle nh;
+Servo steer_servo;
+Servo velocity_servo;
 
-Servo servo;
-Servo esc;
+int output;
 
-int potValue;
 
 void servo_cb(const geometry_msgs::Twist& cmd_msg) {
-  servo.write(cmd_msg.linear.x);
-  digitalWrite(13, HIGH-digitalRead(13));
 
-  potValue = analogRead(A0);
-  potValue = map(potValue, 0, 1023, 0, 180);
-  esc.write(potValue);
+  float steering_angle = cmd_msg.angular.z;
+  float velocity = cmd_msg.linear.x;
+
+  // PWM
+  // velocity_servo.writeMicroseconds(1515);
+
+  if (steering_angle < -5){
+    output = 1400;
+  }else if (steering_angle > 5){
+    output = 1730;
+  }else {
+    output = 1500;
+  }
+
+  // write 1250 ~ 1750 (middle = 1450)
+  steer_servo.writeMicroseconds(output);
+  
+  // -5 < steer < 5
+  if (velocity == 1){
+    velocity_servo.writeMicroseconds(1520);
+  }else if (velocity == 0){
+    velocity_servo.writeMicroseconds(1570);
+  }
 }
 
 ros::Subscriber<geometry_msgs::Twist> sub_servo("controller", servo_cb);
 
 
 void setup() {
-  pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(sub_servo);
-  servo.attach(11);
-  esc.attach(9, 1000, 2000);
+  steer_servo.attach(9);
+  velocity_servo.attach(10);
 }
 
 void loop() {
   nh.spinOnce();
+
   delay(1);
 }
